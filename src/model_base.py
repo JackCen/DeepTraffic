9c42d90f9de39fd528b9905496dbf79afe11628e
jasonzy121 <jasonzy121@gmail.com>
2019-05-21 21:08:27 -0700

    Print to console

--- src/model_base.py
+++ src/model_base.py
@@ -84,6 +84,7 @@ class model(object):
 
 	def sampling_buffer(self):
 		for s in range(self._config.nBufferSample):
-			print("Sample buffer: ", s)
+			if s % 5 == 0:
+				print("Sample buffer: ", s)
 			states, rewards, actions = self.simulate_an_episode(self._config.T, self._action_fn)
 			self._bf.store(states, actions, rewards)
